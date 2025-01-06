9283b22238d3a1f210a96daa0c6c969ad07752ae
barshaul <barshaul@amazon.com>
2025-01-06 08:23:18 +0000

    Python POC: added async API using CFFI

--- python/python/glide/__init__.py
+++ python/python/glide/__init__.py
@@ -154,6 +154,7 @@ from glide.exceptions import (
 )
 from glide.glide_client import GlideClient, GlideClusterClient, TGlideClient
 from glide.logger import Level as LogLevel
+from glide.glide_async_client import GlideAsync
 from glide.logger import Logger
 from glide.routes import (
     AllNodes,
@@ -171,6 +172,7 @@ PubSubMsg = CoreCommands.PubSubMsg
 
 __all__ = [
     # Client
+    "GlideAsync",
     "GlideSync",
     "GlideClient",
     "GlideClusterClient",
