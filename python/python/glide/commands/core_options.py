21008541b61dd8b454b96046dd8840af0426d8c3
Lior Sventitzky <liorsve@amazon.com>
2025-03-31 09:31:31 +0000

    seperated sync module, changed python glide dir structure
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>

---
+++ python/python/glide/commands/core_options.py
@@ -0,0 +1,302 @@
+
+# Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+from datetime import datetime, timedelta
+from enum import Enum
+from typing import List, Optional, Type, Union, get_args
+
+from glide.command_options.command_args import Limit, OrderBy
+from glide.constants import TEncodable
+
+
+class ConditionalChange(Enum):
+    """
+    A condition to the `SET`, `ZADD` and `GEOADD` commands.
+    - ONLY_IF_EXISTS - Only update key / elements that already exist. Equivalent to `XX` in the Valkey API.
+    - ONLY_IF_DOES_NOT_EXIST - Only set key / add elements that does not already exist. Equivalent to `NX` in the Valkey API.
+    """
+
+    ONLY_IF_EXISTS = "XX"
+    ONLY_IF_DOES_NOT_EXIST = "NX"
+
+
+class ExpiryType(Enum):
+    """SET option: The type of the expiry.
+    - SEC - Set the specified expire time, in seconds. Equivalent to `EX` in the Valkey API.
+    - MILLSEC - Set the specified expire time, in milliseconds. Equivalent to `PX` in the Valkey API.
+    - UNIX_SEC - Set the specified Unix time at which the key will expire, in seconds. Equivalent to `EXAT` in the Valkey API.
+    - UNIX_MILLSEC - Set the specified Unix time at which the key will expire, in milliseconds. Equivalent to `PXAT` in the
+        Valkey API.
+    - KEEP_TTL - Retain the time to live associated with the key. Equivalent to `KEEPTTL` in the Valkey API.
+    """
+
+    SEC = 0, Union[int, timedelta]  # Equivalent to `EX` in the Valkey API
+    MILLSEC = 1, Union[int, timedelta]  # Equivalent to `PX` in the Valkey API
+    UNIX_SEC = 2, Union[int, datetime]  # Equivalent to `EXAT` in the Valkey API
+    UNIX_MILLSEC = 3, Union[int, datetime]  # Equivalent to `PXAT` in the Valkey API
+    KEEP_TTL = 4, Type[None]  # Equivalent to `KEEPTTL` in the Valkey API
+
+
+class ExpiryTypeGetEx(Enum):
+    """GetEx option: The type of the expiry.
+    - EX - Set the specified expire time, in seconds. Equivalent to `EX` in the Valkey API.
+    - PX - Set the specified expire time, in milliseconds. Equivalent to `PX` in the Valkey API.
+    - UNIX_SEC - Set the specified Unix time at which the key will expire, in seconds. Equivalent to `EXAT` in the Valkey API.
+    - UNIX_MILLSEC - Set the specified Unix time at which the key will expire, in milliseconds. Equivalent to `PXAT` in the
+        Valkey API.
+    - PERSIST - Remove the time to live associated with the key. Equivalent to `PERSIST` in the Valkey API.
+    """
+
+    SEC = 0, Union[int, timedelta]  # Equivalent to `EX` in the Valkey API
+    MILLSEC = 1, Union[int, timedelta]  # Equivalent to `PX` in the Valkey API
+    UNIX_SEC = 2, Union[int, datetime]  # Equivalent to `EXAT` in the Valkey API
+    UNIX_MILLSEC = 3, Union[int, datetime]  # Equivalent to `PXAT` in the Valkey API
+    PERSIST = 4, Type[None]  # Equivalent to `PERSIST` in the Valkey API
+
+
+class InfoSection(Enum):
+    """
+    INFO option: a specific section of information:
+
+    -SERVER: General information about the server
+    -CLIENTS: Client connections section
+    -MEMORY: Memory consumption related information
+    -PERSISTENCE: RDB and AOF related information
+    -STATS: General statistics
+    -REPLICATION: Master/replica replication information
+    -CPU: CPU consumption statistics
+    -COMMANDSTATS: Valkey command statistics
+    -LATENCYSTATS: Valkey command latency percentile distribution statistics
+    -SENTINEL: Valkey Sentinel section (only applicable to Sentinel instances)
+    -CLUSTER: Valkey Cluster section
+    -MODULES: Modules section
+    -KEYSPACE: Database related statistics
+    -ERRORSTATS: Valkey error statistics
+    -ALL: Return all sections (excluding module generated ones)
+    -DEFAULT: Return only the default set of sections
+    -EVERYTHING: Includes all and modules
+    When no parameter is provided, the default option is assumed.
+    """
+
+    SERVER = "server"
+    CLIENTS = "clients"
+    MEMORY = "memory"
+    PERSISTENCE = "persistence"
+    STATS = "stats"
+    REPLICATION = "replication"
+    CPU = "cpu"
+    COMMAND_STATS = "commandstats"
+    LATENCY_STATS = "latencystats"
+    SENTINEL = "sentinel"
+    CLUSTER = "cluster"
+    MODULES = "modules"
+    KEYSPACE = "keyspace"
+    ERROR_STATS = "errorstats"
+    ALL = "all"
+    DEFAULT = "default"
+    EVERYTHING = "everything"
+
+
+class ExpireOptions(Enum):
+    """
+    EXPIRE option: options for setting key expiry.
+
+    - HasNoExpiry: Set expiry only when the key has no expiry (Equivalent to "NX" in Valkey).
+    - HasExistingExpiry: Set expiry only when the key has an existing expiry (Equivalent to "XX" in Valkey).
+    - NewExpiryGreaterThanCurrent: Set expiry only when the new expiry is greater than the current one (Equivalent
+        to "GT" in Valkey).
+    - NewExpiryLessThanCurrent: Set expiry only when the new expiry is less than the current one (Equivalent to "LT" in Valkey).
+    """
+
+    HasNoExpiry = "NX"
+    HasExistingExpiry = "XX"
+    NewExpiryGreaterThanCurrent = "GT"
+    NewExpiryLessThanCurrent = "LT"
+
+
+class UpdateOptions(Enum):
+    """
+    Options for updating elements of a sorted set key.
+
+    - LESS_THAN: Only update existing elements if the new score is less than the current score.
+    - GREATER_THAN: Only update existing elements if the new score is greater than the current score.
+    """
+
+    LESS_THAN = "LT"
+    GREATER_THAN = "GT"
+
+
+class ExpirySet:
+    """SET option: Represents the expiry type and value to be executed with "SET" command."""
+
+    def __init__(
+        self,
+        expiry_type: ExpiryType,
+        value: Optional[Union[int, datetime, timedelta]],
+    ) -> None:
+        """
+        Args:
+            - expiry_type (ExpiryType): The expiry type.
+            - value (Optional[Union[int, datetime, timedelta]]): The value of the expiration type. The type of expiration
+                determines the type of expiration value:
+                - SEC: Union[int, timedelta]
+                - MILLSEC: Union[int, timedelta]
+                - UNIX_SEC: Union[int, datetime]
+                - UNIX_MILLSEC: Union[int, datetime]
+                - KEEP_TTL: Type[None]
+        """
+        self.set_expiry_type_and_value(expiry_type, value)
+
+    def set_expiry_type_and_value(
+        self, expiry_type: ExpiryType, value: Optional[Union[int, datetime, timedelta]]
+    ):
+        if not isinstance(value, get_args(expiry_type.value[1])):
+            raise ValueError(
+                f"The value of {expiry_type} should be of type {expiry_type.value[1]}"
+            )
+        self.expiry_type = expiry_type
+        if self.expiry_type == ExpiryType.SEC:
+            self.cmd_arg = "EX"
+            if isinstance(value, timedelta):
+                value = int(value.total_seconds())
+        elif self.expiry_type == ExpiryType.MILLSEC:
+            self.cmd_arg = "PX"
+            if isinstance(value, timedelta):
+                value = int(value.total_seconds() * 1000)
+        elif self.expiry_type == ExpiryType.UNIX_SEC:
+            self.cmd_arg = "EXAT"
+            if isinstance(value, datetime):
+                value = int(value.timestamp())
+        elif self.expiry_type == ExpiryType.UNIX_MILLSEC:
+            self.cmd_arg = "PXAT"
+            if isinstance(value, datetime):
+                value = int(value.timestamp() * 1000)
+        elif self.expiry_type == ExpiryType.KEEP_TTL:
+            self.cmd_arg = "KEEPTTL"
+        self.value = str(value) if value else None
+
+    def get_cmd_args(self) -> List[str]:
+        return [self.cmd_arg] if self.value is None else [self.cmd_arg, self.value]
+
+
+class ExpiryGetEx:
+    """GetEx option: Represents the expiry type and value to be executed with "GetEx" command."""
+
+    def __init__(
+        self,
+        expiry_type: ExpiryTypeGetEx,
+        value: Optional[Union[int, datetime, timedelta]],
+    ) -> None:
+        """
+        Args:
+            - expiry_type (ExpiryType): The expiry type.
+            - value (Optional[Union[int, datetime, timedelta]]): The value of the expiration type. The type of expiration
+                determines the type of expiration value:
+                - SEC: Union[int, timedelta]
+                - MILLSEC: Union[int, timedelta]
+                - UNIX_SEC: Union[int, datetime]
+                - UNIX_MILLSEC: Union[int, datetime]
+                - PERSIST: Type[None]
+        """
+        self.set_expiry_type_and_value(expiry_type, value)
+
+    def set_expiry_type_and_value(
+        self,
+        expiry_type: ExpiryTypeGetEx,
+        value: Optional[Union[int, datetime, timedelta]],
+    ):
+        if not isinstance(value, get_args(expiry_type.value[1])):
+            raise ValueError(
+                f"The value of {expiry_type} should be of type {expiry_type.value[1]}"
+            )
+        self.expiry_type = expiry_type
+        if self.expiry_type == ExpiryTypeGetEx.SEC:
+            self.cmd_arg = "EX"
+            if isinstance(value, timedelta):
+                value = int(value.total_seconds())
+        elif self.expiry_type == ExpiryTypeGetEx.MILLSEC:
+            self.cmd_arg = "PX"
+            if isinstance(value, timedelta):
+                value = int(value.total_seconds() * 1000)
+        elif self.expiry_type == ExpiryTypeGetEx.UNIX_SEC:
+            self.cmd_arg = "EXAT"
+            if isinstance(value, datetime):
+                value = int(value.timestamp())
+        elif self.expiry_type == ExpiryTypeGetEx.UNIX_MILLSEC:
+            self.cmd_arg = "PXAT"
+            if isinstance(value, datetime):
+                value = int(value.timestamp() * 1000)
+        elif self.expiry_type == ExpiryTypeGetEx.PERSIST:
+            self.cmd_arg = "PERSIST"
+        self.value = str(value) if value else None
+
+    def get_cmd_args(self) -> List[str]:
+        return [self.cmd_arg] if self.value is None else [self.cmd_arg, self.value]
+
+
+class InsertPosition(Enum):
+    BEFORE = "BEFORE"
+    AFTER = "AFTER"
+
+
+class FlushMode(Enum):
+    """
+    Defines flushing mode for:
+
+    `FLUSHALL` command and `FUNCTION FLUSH` command.
+
+    See https://valkey.io/commands/flushall/ and https://valkey.io/commands/function-flush/ for details
+
+    SYNC was introduced in version 6.2.0.
+    """
+
+    ASYNC = "ASYNC"
+    SYNC = "SYNC"
+
+
+class FunctionRestorePolicy(Enum):
+    """
+    Options for the FUNCTION RESTORE command.
+
+    - APPEND: Appends the restored libraries to the existing libraries and aborts on collision. This is the
+        default policy.
+    - FLUSH: Deletes all existing libraries before restoring the payload.
+    - REPLACE: Appends the restored libraries to the existing libraries, replacing any existing ones in case
+        of name collisions. Note that this policy doesn't prevent function name collisions, only libraries.
+    """
+
+    APPEND = "APPEND"
+    FLUSH = "FLUSH"
+    REPLACE = "REPLACE"
+
+
+def _build_sort_args(
+    key: TEncodable,
+    by_pattern: Optional[TEncodable] = None,
+    limit: Optional[Limit] = None,
+    get_patterns: Optional[List[TEncodable]] = None,
+    order: Optional[OrderBy] = None,
+    alpha: Optional[bool] = None,
+    store: Optional[TEncodable] = None,
+) -> List[TEncodable]:
+    args = [key]
+
+    if by_pattern:
+        args.extend(["BY", by_pattern])
+
+    if limit:
+        args.extend(["LIMIT", str(limit.offset), str(limit.count)])
+
+    if get_patterns:
+        for pattern in get_patterns:
+            args.extend(["GET", pattern])
+
+    if order:
+        args.append(order.value)
+
+    if alpha:
+        args.append("ALPHA")
+
+    if store:
+        args.extend(["STORE", store])
+
+    return args
---
+++ python/python/glide/sync/glide_client.py
@@ -0,0 +1,297 @@
+# Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+from cffi import FFI
+import sys
+from glide.protobuf.command_request_pb2 import Command, CommandRequest, RequestType
+from typing import List, Union, Optional, cast
+from glide.sync.sync_commands.core import CoreCommands
+from glide.sync.sync_commands.cluster_commands import ClusterCommands
+from glide.sync.sync_commands.standalone_commands import StandaloneCommands
+from glide.constants import DEFAULT_READ_BYTES_SIZE, OK, TEncodable, TRequest, TResult
+from glide.routes import Route
+from glide.exceptions import ClosingError, RequestError
+from glide.config import BaseClientConfiguration, GlideClientConfiguration, GlideClusterClientConfiguration
+from glide.glide_client import get_request_error_class
+if sys.version_info >= (3, 11):
+    from typing import Self
+
+
+# Enum values must match the Rust definition
+class FFIClientTypeEnum:
+    Async = 0
+    Sync = 1
+
+
+class BaseClient(CoreCommands):    
+    
+    def __init__(self, config: BaseClientConfiguration):
+            """
+            To create a new client, use the `create` classmethod
+            """
+            self.config: BaseClientConfiguration = config
+            
+    @classmethod
+    def create(cls, config: BaseClientConfiguration) -> Self:
+        self = cls(config)
+        self._init_ffi()
+        self.config = config
+        conn_req = config._create_a_protobuf_conn_request(cluster_mode=type(config) == GlideClusterClientConfiguration)
+        conn_req_bytes = conn_req.SerializeToString()
+        client_type = self.ffi.new("ClientType*", {
+            "_type": self.ffi.cast("ClientTypeEnum", FFIClientTypeEnum.Sync),
+        })
+        client_response_ptr = self.lib.create_client(conn_req_bytes, len(conn_req_bytes), client_type) 
+        # Handle the connection response
+        if client_response_ptr != self.ffi.NULL:
+            client_response = self.ffi.cast("ConnectionResponse*", client_response_ptr)
+            if client_response.conn_ptr != self.ffi.NULL:
+                self.core_client = client_response.conn_ptr
+            else:
+                error_message = self.ffi.string(client_response.connection_error_message).decode('utf-8') if client_response.connection_error_message != self.ffi.NULL else "Unknown error"
+                raise ClosingError(error_message)
+
+            # Free the connection response to avoid memory leaks
+            self.lib.free_connection_response(client_response_ptr)
+        else:
+            raise ClosingError("Failed to create client, response pointer is NULL.")
+        return self
+
+    def _init_ffi(self):
+        self.ffi = FFI()
+
+        # Define the CommandResponse struct and related types
+        self.ffi.cdef("""
+            typedef struct {
+                int response_type;
+                long int_value;
+                double float_value;
+                bool bool_value;
+                char* string_value;
+                long string_value_len;
+                struct CommandResponse* array_value;
+                long array_value_len;
+                struct CommandResponse* map_key;
+                struct CommandResponse* map_value;
+                struct CommandResponse* sets_value;
+                long sets_value_len;
+            } CommandResponse;
+
+            typedef enum {
+                Null = 0,
+                Int = 1,
+                Float = 2,
+                Bool = 3,
+                String = 4,
+                Array = 5,
+                Map = 6,
+                Sets = 7
+            } ResponseType;
+
+            typedef void (*SuccessCallback)(uintptr_t index_ptr, const CommandResponse* message);
+            typedef void (*FailureCallback)(uintptr_t index_ptr, const char* error_message, int error_type);
+
+            typedef struct {
+                const void* conn_ptr;
+                const char* connection_error_message;
+            } ConnectionResponse;
+
+            typedef struct {
+                const char* command_error_message;
+                int command_error_type;
+            } CommandError;
+
+            typedef struct {
+                CommandResponse* response;
+                CommandError* command_error;
+            } CommandResult;
+
+            typedef enum {
+                Async = 0,
+                Sync = 1
+            } ClientTypeEnum;
+
+            typedef struct {
+                SuccessCallback success_callback;
+                FailureCallback failure_callback;
+            } AsyncClient;
+
+            typedef struct {
+                int _type;  // Enum to differentiate between Async and Sync
+                union {
+                    struct {
+                        void (*success_callback)(uintptr_t, const void*);
+                        void (*failure_callback)(uintptr_t, const char*, int);
+                    } async_client;
+                };
+            } ClientType;
+
+            // Function declarations
+            const ConnectionResponse* create_client(
+                const uint8_t* connection_request_bytes,
+                size_t connection_request_len,
+                const ClientType* client_type  // Pass by pointer
+            );
+            void close_client(const void* client_adapter_ptr);
+            void free_connection_response(ConnectionResponse* connection_response_ptr);
+            char* get_response_type_string(int response_type);
+            void free_response_type_string(char* response_string);
+            void free_command_response(CommandResponse* command_response_ptr);
+            void free_error_message(char* error_message);
+            void free_command_result(CommandResult* command_result_ptr);
+            CommandResult* command(const void* client_adapter_ptr, uintptr_t channel, int command_type, unsigned long arg_count, const size_t *args, const unsigned long* args_len, const unsigned char* route_bytes, size_t route_bytes_len);
+
+        """)
+
+        # Load the shared library (adjust the path to your compiled Rust library)
+        self.lib = self.ffi.dlopen("/home/ubuntu/glide-for-redis/go/target/debug/libglide_rs.so")
+        
+    def _handle_response(self, message):
+        if message == self.ffi.NULL:
+            raise RequestError("Received NULL message.")
+
+        # Identify the type of the message
+        message_type = self.ffi.typeof(message).cname
+
+        # If message is a pointer to CommandResponse, dereference it
+        if message_type == "CommandResponse *":
+            message = message[0]  # Dereference the pointer
+            message_type = self.ffi.typeof(message).cname
+        # Check if message is now a CommandResponse
+        if message_type == "CommandResponse":
+            msg = message
+            if msg.response_type == 0:  # Null
+                return None
+            elif msg.response_type == 1:  # Int
+                return msg.int_value
+            elif msg.response_type == 2:  # Float
+                return msg.float_value
+            elif msg.response_type == 3:  # Bool
+                return bool(msg.bool_value)
+            elif msg.response_type == 4:  # String
+                try:
+                    string_value = self.ffi.buffer(msg.string_value, msg.string_value_len)[:]
+                    return string_value
+                except Exception as e:
+                    # TODO: Add memory cleanup in case of failures
+                    raise RequestError(f"Error decoding string value: {e}")
+            elif msg.response_type == 5:  # Array
+                array = []
+                for i in range(msg.array_value_len):
+                    element = self.ffi.cast("struct CommandResponse*", msg.array_value + i)
+                    array.append(self._handle_response(element))
+                return array
+            elif msg.response_type == 6:  # Map
+                map_dict = {}
+                for i in range(msg.array_value_len):
+                    key = self.ffi.cast("struct CommandResponse*", msg.map_key + i)
+                    value = self.ffi.cast("struct CommandResponse*", msg.map_value + i)
+                    map_dict[self._handle_response(key)] = self._handle_response(value)
+                return map_dict
+            elif msg.response_type == 7:  # Sets
+                result_set = set()
+                sets_array = self.ffi.cast(f"struct CommandResponse[{msg.sets_value_len}]", msg.sets_value)
+                for i in range(msg.sets_value_len):
+                    element = sets_array[i]  # Already a struct
+                    result_set.add(self._handle_response(element))
+                return result_set
+            else:
+                raise RequestError(f"Unhandled response type = {msg.response_type}")
+        else:
+            raise RequestError(f"Unexpected message type = {message_type}")
+  
+        
+
+    def _to_c_strings(self, args):
+        """Convert Python arguments to C-compatible pointers and lengths."""
+        c_strings = []
+        string_lengths = []
+        buffers = []  # Keep a reference to prevent premature garbage collection
+
+        for arg in args:
+            if isinstance(arg, str):
+                # Convert string to UTF-8 bytes
+                arg_bytes = arg.encode('utf-8')
+            elif isinstance(arg, (int, float)):
+                # Convert numeric values to strings and then to bytes
+                arg_bytes = str(arg).encode('utf-8')
+            elif isinstance (arg, bytes):
+                arg_bytes = arg
+            else:
+                raise ValueError(f"Unsupported argument type: {type(arg)}")
+
+            # Use ffi.from_buffer for zero-copy conversion
+            buffers.append(arg_bytes)  # Keep the byte buffer alive
+            c_strings.append(self.ffi.cast("size_t", self.ffi.from_buffer(arg_bytes)))
+            string_lengths.append(len(arg_bytes))
+        # Return C-compatible arrays and keep buffers alive
+        return (
+            self.ffi.new("size_t[]", c_strings),
+            self.ffi.new("unsigned long[]", string_lengths),
+            buffers,  # Ensure buffers stay alive
+        )
+    
+    def _handle_cmd_result(self, command_result):
+        try:
+            if command_result == self.ffi.NULL:
+                raise ClosingError("Internal error: Received NULL as a command result")
+            if command_result.command_error != self.ffi.NULL:
+                # Handle the error case
+                error = self.ffi.cast("CommandError*", command_result.command_error)
+                error_message = self.ffi.string(error.command_error_message).decode('utf-8')
+                error_class = get_request_error_class(error.command_error_type)
+                # Free the error message to avoid memory leaks
+                raise error_class(error_message)
+            else:
+                return self._handle_response(command_result.response)
+                # Free the error message to avoid memory leaks
+        finally:
+            self.lib.free_command_result(command_result)
+
+    def _execute_command(
+        self,
+        request_type: RequestType.ValueType,
+        args: List[TEncodable],
+        route: Optional[Route] = None,
+    ) -> TResult:
+        client_adapter_ptr = self.core_client
+        if client_adapter_ptr == self.ffi.NULL:
+            raise ValueError("Invalid client pointer.")
+
+        # Convert the arguments to C-compatible pointers
+        c_args, c_lengths, buffers = self._to_c_strings(args)
+        # Call the command function
+        route_bytes = b"" # TODO: add support for route 
+        route_ptr = self.ffi.new("unsigned char[]", route_bytes)
+
+
+        result = self.lib.command(
+            client_adapter_ptr,  # Client pointer
+            1,  # Example channel (adjust as needed)
+            request_type,  # Request type (e.g., GET or SET)
+            len(args),  # Number of arguments
+            c_args,  # Array of argument pointers
+            c_lengths,  # Array of argument lengths
+            route_ptr,
+            len(route_bytes)
+        )
+        return self._handle_cmd_result(result)
+    
+    def close(self):
+        self.lib.close_client(self.core_client)
+    
+class GlideClusterClient(BaseClient, ClusterCommands):
+    """
+    Client used for connection to cluster servers.
+    For full documentation, see
+    https://github.com/valkey-io/valkey-glide/wiki/Python-wrapper#cluster
+    """
+
+
+class GlideClient(BaseClient, StandaloneCommands):
+    """
+    Client used for connection to standalone servers.
+    For full documentation, see
+    https://github.com/valkey-io/valkey-glide/wiki/Python-wrapper#standalone
+    """
+
+TGlideClient = Union[GlideClient, GlideClusterClient]
