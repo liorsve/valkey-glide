324fa4872b6721d2fcdb7a21590820afd2bed58a
Lior Sventitzky <liorsve@amazon.com>
2025-05-20 10:22:16 +0000

    add mike
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -17,7 +17,8 @@ function install_mkdocs() {
             mkdocstrings-python==1.13.0        \
             pymdown-extensions                 \
             mkdocs-breadcrumbs-plugin          \
-            mkdocs-material
+            mkdocs-material                    \
+            mike
         echo "-- Done"
     fi
     command -v mkdocs
@@ -30,7 +31,30 @@ function build_docs() {
     # Python - should be last, since Python docs are generated using mkdocs plugin
     # Set PYTHONPATH so python classes are found
     export PYTHONPATH=${BASE_DIR}/python/python:$PYTHONPATH
-    (cd ${BASE_DIR}/docs && python3 -m mkdocs ${TARGET})
+    
+    if [[ "$TARGET" == "serve" ]]; then
+        (cd ${BASE_DIR}/docs && mike serve)
+    elif [[ "$TARGET" == *"deploy"* ]]; then
+        # Get version from environment variables set by GitHub Actions
+        VERSION=${DOCS_VERSION:?Version must be specified}
+        SET_DEFAULT=${DOCS_SET_DEFAULT:-false}
+        
+        echo "Deploying documentation version: $VERSION"
+        
+        # Build the mike command
+        MIKE_CMD="mike deploy --push $VERSION"
+        
+        # Deploy the version
+        (cd ${BASE_DIR}/docs && $MIKE_CMD)
+        
+        # Set as default version if requested
+        if [[ "$SET_DEFAULT" == "true" ]]; then
+            echo "Setting $VERSION as default version"
+            (cd ${BASE_DIR}/docs && mike set-default --push $VERSION)
+        fi
+    # else
+    #     (cd ${BASE_DIR}/docs && python3 -m mkdocs ${TARGET})
+    fi
 }
 
 install_mkdocs
